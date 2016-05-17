1815b70054934539a1e34f3453e7b99602a922ee
Aaron Marburg <amarburg@apl.washington.edu>
2016-05-17 15:55:17 -0700

    Have basic optical flow algorithm plugged in.

--- tools/MotionTrack.cpp
+++ tools/MotionTrack.cpp
@@ -1,7 +1,10 @@
 #include <string>
+#include <vector>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/video.hpp>
 
 #include <boost/filesystem.hpp>
 #include <tclap/CmdLine.h>
@@ -15,10 +18,13 @@ class MotionTracking
 {
 public:
 	MotionTracking( const fs::path &inFile )
-		: _inputFilePath( inFile ), _capture( inFile.string() ),
+		: _inputFilePath( inFile ),
+			_flowVideoPath(),
+			_capture( inFile.string() ),
 			_stride(10), _skip(0),
 			_doDisplay( false ),
-			_waitKey(-1)
+			_waitKey(-1),
+			_opticalFlow( createOptFlow_DualTVL1() )
 	{}
 
 	~MotionTracking()
@@ -30,6 +36,7 @@ public:
 	unsigned int setSkip( unsigned int c ) { return _skip=c; }
 	bool setDoDisplay( bool d ) { return _doDisplay=d; }
 	int setWait( int w ) { return _waitKey = w; }
+	void setFlowVideoOutput( const fs::path &p ) { _flowVideoPath = p; }
 
 	bool operator()( void )
 	{
@@ -37,6 +44,17 @@ public:
 			cerr << "Unable to open video file.  Aborting." << endl;
 			return false;
 		}
+
+		if( _flowVideoPath.size() > 0 ) {
+			cout << "Saving flow video to " << _flowVideoPath.string() << endl;
+			_flowVideo.open( _flowVideoPath.string(), int(_capture.get(CV_CAP_PROP_FOURCC)), fps(), frameSize(), true  );
+
+			if( !_flowVideo.isOpened() ) {
+				cerr << "Unabled to open output video " << _flowVideoPath.string() << ", aborting";
+				return false;
+			}
+		}
+
 
 		cv::Size sz( frameSize() );
 		cout << "Video frames are " << sz.width << " x " << sz.height << endl;
@@ -58,15 +76,51 @@ public:
 			}
 		}
 
-		Mat current, prev;
+		Mat current, prevGray;
 		while( _capture.read(current) ) {
+
+			Mat curGray, mag, angle;
+
+			cvtColor( current, curGray, CV_BGR2GRAY );
+
+			if( !prevGray.empty() ) {
+				Mat flow;
+				_opticalFlow->calc( prevGray, curGray, flow );
+
+				std::vector<cv::Mat> components(2);
+				split( flow, components );
+
+				cartToPolar( components[0], components[1], mag, angle );
+			}
+
+			if( !mag.empty() && (_flowVideo.isOpened() || _doDisplay) ) {
+				Mat composite( cv::Size(2*mag.cols, mag.rows), CV_8UC3 );
+				Mat magRoi( composite, cv::Rect(0,0, mag.cols, mag.rows ) ),
+						angleRoi( composite, cv::Rect( mag.cols, 0, angle.cols, angle.rows ));
+
+				double magMin, magMax = 1;
+				minMaxLoc( mag, &magMin, &magMax );
+
+				cvtColor( mag/magMax, magRoi, CV_GRAY2BGR );
+				cvtColor( angle / 2*M_PI, angleRoi, CV_GRAY2BGR );
+
+				if( _doDisplay) {
+					imshow("Magnitude", magRoi );
+					imshow("Angle", angleRoi );
+				}
+
+				if( _flowVideo.isOpened() )
+					_flowVideo << composite;
+			}
 
 			if( _doDisplay) {
 				imshow("MotionTracking", current );
+
 				waitKey( _waitKey );
 			}
 
-			prev = current;
+			prevGray = curGray;
+
 
 			if( _stride > 1 ) {
 				for( auto i = 0; i < (_stride-1); ++i ) {
@@ -95,11 +149,14 @@ public:
 
 protected:
 
-	fs::path _inputFilePath;
+	fs::path _inputFilePath, _flowVideoPath;
 	VideoCapture _capture;
+	VideoWriter _flowVideo;
 	unsigned int _stride, _skip;
 	bool _doDisplay;
 	int _waitKey;
+
+Ptr<DenseOpticalFlow> _opticalFlow;
 
 };
 
@@ -115,6 +172,7 @@ int main( int argc, char ** argv )
 		TCLAP::SwitchArg doDisplayArg("x","display","Print name backwards", cmd, true);
 		TCLAP::ValueArg< int> waitKeyArg("","wait-key","Number of frames",false,-1,"Number of frames", cmd);
 
+		TCLAP::ValueArg<string> flowVideoOutput("","magnitude-video","",false,"","", cmd);
 
 		TCLAP::UnlabeledValueArg<string> filenameArg("input-file", "Input file", true, "", "Input filename", cmd );
 
@@ -132,6 +190,7 @@ int main( int argc, char ** argv )
 		mt.setStride( strideArg.getValue() );
 		mt.setDoDisplay( doDisplayArg.getValue() );
 		mt.setWait( waitKeyArg.getValue() );
+		mt.setFlowVideoOutput( flowVideoOutput.getValue() );
 		mt();
 
 
