161d1081fa8a5c54a16c5fb3619e143300099dbc
Aaron Marburg <amarburg@apl.washington.edu>
2018-01-30 10:24:53 -0800

    Change if/exit in launch_worker into a while/sleep loop.

--- launch_worker.sh
+++ launch_worker.sh
@@ -2,9 +2,9 @@
 
 echo "launch_worker.sh"
 
-if [ ! -f /output/CamHD_motion_metadata/README.md ]; then
+while [ ! -f /output/CamHD_motion_metadata/README.md ]; do
   echo "Could not find output directory /output/CamHD_motion_metadata"
-  exit
-fi
+  sleep 5
+done
 
 /code/camhd_motion_analysis/python/rq_worker.py "$@"
