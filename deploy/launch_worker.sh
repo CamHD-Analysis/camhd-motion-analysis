7ba97ba02f59d96bdd1f8603bbb34453fb00fa98
Aaron Marburg <amarburg@notetofutureself.org>
2018-07-17 15:27:25 -0700

    Minor refinement to Dockerfiles

--- deploy/launch_worker.sh
+++ deploy/launch_worker.sh
@@ -3,11 +3,20 @@
 echo "launch_worker.sh"
 
 
+DATA_DIR=${OUTPUT_DIR:-"/output/CamHD_motion_metadata"}
+
 ## Check for presence of properly mounted CamHD_motion_metadata before
 ## proceeding
-if [ ! -f $OUTPUT_DIR/README.md ]; then
-  echo "Could not find output directory $OUTPUT_DIR"
+echo "Using CamHD_motion_metadata mounted at ${DATA_DIR}"
+
+if [ ! -f $DATA_DIR/README.md ]; then
+  echo "Could not find output directory $DATA_DIR"
   exit
+fi
+
+if [ ! -z "${DELAY_START}" ]; then
+  echo "Delaying start for ${DELAY_START} seconds"
+  sleep ${DELAY_START}
 fi
 
 /code/camhd-motion-analysis/python/rq_worker.py "$@"
